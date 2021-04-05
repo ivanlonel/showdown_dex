d7929d0d406e9de86e4c53af5babe7cab4ef3a6c
ivanlonel <ivanlonel91@gmail.com>
2021-04-05 13:28:12 -0300

    Pass paramenters as arguments to main()

--- smogon_analyses.py
+++ smogon_analyses.py
@@ -9,6 +9,7 @@ import aiofiles
 import tenacity
 from logging_queue import log_via_queue
 
+
 def load_nested_strings_as_json(jso):
     if isinstance(jso, dict):
         jso = {k: load_nested_strings_as_json(v) for k, v in jso.items()}
@@ -21,61 +22,62 @@ def load_nested_strings_as_json(jso):
             jso = json.dumps(jso, ensure_ascii=False)[1:-1]  # escape \ and ".
     return jso
 
+
 @tenacity.retry(
                 retry = tenacity.retry_if_exception_type(aiohttp.ClientError)
                       | tenacity.retry_if_exception_type(asyncio.TimeoutError),
                 wait = tenacity.wait_random_exponential(multiplier=1.875, max=60),
                 stop = tenacity.stop_after_attempt(7),
                 reraise = True,
-                before_sleep = tenacity.before_sleep.before_sleep_log(logging.getLogger(), logging.DEBUG, exc_info=False)
+                before_sleep = tenacity.before_sleep.before_sleep_log(logging.getLogger(), logging.DEBUG)
                )
 async def fetch(session, url, json=None):
     async with session.post(url, json=json, raise_for_status=True) as response:
         return load_nested_strings_as_json(await response.json())
 
+
 async def bound_fetch(session, semaphore, url, json=None):
     async with semaphore:
         return {'request': json, 'response': await fetch(session, url, json=json)}
 
+
 def to_alias(name):
     return regex.sub(r'[^a-z0-9\-]+', '', name.lower().replace(' ', '-'))
 
-async def main():
-    URL = 'https://www.smogon.com/dex/_rpc'
-    LANGUAGE = 'en'
-    MAX_SIM_CONNS = 64
+
+async def main(url='https://www.smogon.com/dex/_rpc', lang='en', max_sim_conns=64):
 
     pathlib.Path('json').mkdir(exist_ok=True)
 
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=60)) as session:
-        json_gens = await fetch(session, f'{URL}/dump-gens')
+        json_gens = await fetch(session, f'{url}/dump-gens')
 
         gens = [gen['shorthand'] for gen in json_gens]
 
         logging.debug(f'Gens: {gens}')
 
         async with aiofiles.open('json/smogon_gens.json', 'w', newline='', encoding='utf-8') as fd:
-            #await fd.write(ujson.dumps(json_gens, ensure_ascii=False, indent='\t'))
+            # await fd.write(ujson.dumps(json_gens, ensure_ascii=False, indent=2))
             await fd.write(json.dumps(json_gens, ensure_ascii=False, separators=(',', ':')))
 
-        sem = asyncio.Semaphore(MAX_SIM_CONNS)
+        sem = asyncio.Semaphore(max_sim_conns)
 
-        tasks_basics = (asyncio.create_task(bound_fetch(session, sem, f'{URL}/dump-basics', json={'gen': gen.lower()})) for gen in gens)
+        tasks_basics = (asyncio.create_task(bound_fetch(session, sem, f'{url}/dump-basics', json={'gen': gen.lower()})) for gen in gens)
         json_basics = [{**item['request'], **item['response']} for item in await asyncio.gather(*tasks_basics)]
 
         async with aiofiles.open('json/smogon_basics.json', 'w', newline='', encoding='utf-8') as fd:
-            #await fd.write(ujson.dumps(json_basics, ensure_ascii=False, indent='\t'))
+            # await fd.write(ujson.dumps(json_basics, ensure_ascii=False, indent=2))
             await fd.write(json.dumps(json_basics, ensure_ascii=False, separators=(',', ':')))
 
-        tasks_pokemon = (asyncio.create_task(bound_fetch(session, sem, f'{URL}/dump-pokemon', json={'gen': gen_info['gen'], 'language': LANGUAGE, 'alias': to_alias(pokemon['name'])})) for gen_info in json_basics for pokemon in gen_info['pokemon'])
+        tasks_pokemon = (asyncio.create_task(bound_fetch(session, sem, f'{url}/dump-pokemon', json={'gen': gen_info['gen'], 'language': lang, 'alias': to_alias(pokemon['name'])})) for gen_info in json_basics for pokemon in gen_info['pokemon'])
         json_pokemon = [{**item['request'], **item['response']} for item in await asyncio.gather(*tasks_pokemon) if item['response'] is not None]
 
         # For each analysis in json_pokemon, look up in which other languages there are analyses for that pokémon/gen and fetch them as well.
-        tasks_multilang = (asyncio.create_task(bound_fetch(session, sem, f'{URL}/dump-pokemon', json={'gen': analysis['gen'], 'language': lang, 'alias': analysis['alias']})) for analysis in json_pokemon for lang in analysis['languages'] if lang != LANGUAGE)
+        tasks_multilang = (asyncio.create_task(bound_fetch(session, sem, f'{url}/dump-pokemon', json={'gen': analysis['gen'], 'language': lang, 'alias': analysis['alias']})) for analysis in json_pokemon for lang in analysis['languages'] if lang != lang)
         json_multilang = [{**item['request'], **item['response']} for item in await asyncio.gather(*tasks_multilang) if item['response'] is not None]
 
     async with aiofiles.open('json/smogon_analyses.json', 'w', newline='', encoding='utf-8') as fd:
-        #await fd.write(ujson.dumps([*json_pokemon, *json_multilang], ensure_ascii=False, indent='\t'))
+        # await fd.write(ujson.dumps([*json_pokemon, *json_multilang], ensure_ascii=False, indent=2))
         await fd.write(json.dumps([*json_pokemon, *json_multilang], ensure_ascii=False, separators=(',', ':')))
 
     logging.debug(f'Analyses downloaded: {len(json_pokemon) + len(json_multilang)}')
@@ -87,7 +89,8 @@ if __name__ == '__main__':
     try:
         import uvloop  # Unavailable on Windows, optional on Unix.
     except ModuleNotFoundError:
-        # aiohttp 3 raises RuntimeError('Event loop is closed') at the end on Windows if using ProactorEventLoop (which is the default on Windows in Python 3.8+).
+        # aiohttp 3 raises RuntimeError('Event loop is closed') at the end on Windows if using ProactorEventLoop
+        # (which is the default on Windows in Python 3.8+).
         if sys.platform.startswith('win') and sys.version_info[:2] >= (3, 8) and int(aiohttp.__version__.split('.')[0]) < 4:
             # Force use of SelectorEventLoop
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
